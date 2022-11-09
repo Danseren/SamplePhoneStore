56e5b3628f17f95b4c29b5cbfdb19924be48ad14
Danseren <104249031+Danseren@users.noreply.github.com>
2022-11-09 15:58:03 +0500

    Добавил Binding, BottomNavigation

--- app/src/main/java/ru/sample/store/denis/MainActivity.kt
+++ app/src/main/java/ru/sample/store/denis/MainActivity.kt
@@ -3,12 +3,17 @@ package ru.sample.store.denis
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.sample.store.denis.ui.main.MainFragment
+import ru.sample.store.denis.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
