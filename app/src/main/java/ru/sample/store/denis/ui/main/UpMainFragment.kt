0315bc4e0990cdf67df7e0043b41c4557efa563c
Danseren <104249031+Danseren@users.noreply.github.com>
2022-11-14 22:20:37 +0500

    Наметки дизайна. Добавил AlertDialog по нажатию на фильтр

--- app/src/main/java/ru/sample/store/denis/ui/main/UpMainFragment.kt
+++ app/src/main/java/ru/sample/store/denis/ui/main/UpMainFragment.kt
@@ -1,12 +1,18 @@
 package ru.sample.store.denis.ui.main
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import ru.sample.store.denis.R
 import ru.sample.store.denis.databinding.FragmentUpMainBinding
+import ru.sample.store.denis.databinding.MessageBoxBinding
 import ru.sample.store.denis.model.DataCardBestSeller
 import ru.sample.store.denis.model.DataCardHotSales
 
@@ -42,8 +48,36 @@ class UpMainFragment: Fragment() {
             DataCardBestSeller(R.drawable.samsung_note_20_ultra, "Samsung Note 20 Ultra", "1,500", "1,047", true ),
             DataCardBestSeller(R.drawable.motorola_one_edge, "Motorola One Edge ", "400", "300", true )
         )
+
         binding.recyclerViewHotSales.adapter = RecyclerCardHotSalesAdapter(dataCardHotSales)
         binding.recyclerViewBestSellers.adapter = RecyclerCardBestSellerAdapter(dataCardBestSeller)
+
+        binding.iconFilter.setOnClickListener {
+            showMessageBox()
+        }
+    }
+
+    @SuppressLint("RestrictedApi")
+    fun showMessageBox(){
+
+        //Inflate the dialog as custom view
+        val messageBoxView = LayoutInflater.from(activity).inflate(R.layout.message_box, null)
+
+        //AlertDialogBuilder
+        //val messageBoxBuilder = AlertDialog.Builder(activity).setView(messageBoxView)
+        val messageBoxBuilder = MaterialAlertDialogBuilder(requireActivity(), R.style.MaterialAlertDialog_rounded).setView(messageBoxView)
+
+        //show dialog
+        val  messageBoxInstance = messageBoxBuilder.show()
+        messageBoxInstance.window?.setGravity(Gravity.BOTTOM)
+        messageBoxInstance.window?.setLayout(MATCH_PARENT, WRAP_CONTENT)
+
+        //set Listener
+        messageBoxView.setOnClickListener(){
+            //close dialog
+            messageBoxInstance.dismiss()
+        }
+
     }
 
     override fun onDestroyView() {
